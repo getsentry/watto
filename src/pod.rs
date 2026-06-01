69e4ad408d3128461571b68359085d4fa07a9dfe
tobias-wilfert <tobias.wilfert@sentry.io>
2026-06-01 15:38:54 -0400

    lint

--- src/pod.rs
+++ src/pod.rs
@@ -78,7 +78,7 @@ pub unsafe trait Pod {
         let len = bytes.len();
         let elem_size = mem::size_of::<Self>();
 
-        if len % elem_size != 0 || !crate::utils::is_aligned_to(bytes, mem::align_of::<Self>()) {
+        if !len.is_multiple_of(elem_size) || !crate::utils::is_aligned_to(bytes, mem::align_of::<Self>()) {
             return None;
         }
 
